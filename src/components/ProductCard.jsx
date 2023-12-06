6b0448d1fe33136fa5b3b5b1990f0e94ced45aec
Amandeep Singh <a.d.scr71234@gmail.com>
2023-12-06 08:03:08 +0530

    made product list grid layout responsive

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,36 +7,68 @@ export default function ProductCard({ id, title, img, price }) {
       {!img ? (
         <Loader />
       ) : (
-        <article className="col mb-sm-3 container-fluid" key={id}>
+        <Link
+          to={`/shop/${id}`}
+          className="card product-list-card
+                       border shadow p-3 mb-5 bg-body rounded
+                       search-card-container mb-5
+                       
+                      "
+          style={{
+            marginInline: "auto",
+            width: "min(90%, 60rem)",
+            marginBlock: "1.75rem",
+          }}
+          key={id}
+        >
+          <img
+            src={img}
+            className="card-img-top product-card-img mb-md-3"
+            alt={title}
+          />
           <div
-            className="p-0 container-lg product-card-container d-flex justify-content-center align-items-center
-           shadow   bg-body rounded
+            className="card-body result-card-body
+            d-md-flex flex-column
           "
           >
-            <Link to={id} style={{ textDecoration: "none", color: "#000" }}>
-              <div
-                className="card"
-                style={{
-                  minWidth: "250px",
-                  minHeight: "430px",
-                  border: "none",
-                }}
-              >
-                <img
-                  src={img}
-                  className="card-img-top product-img"
-                  alt={title}
-                />
-                <div className="card-body">
-                  <small className="card-title">{title}</small>
-                  <p className="pt-1 pb-2 fw-bold card-text product-price">
-                    &#8377;{price}.00
-                  </p>
-                </div>
-              </div>
-            </Link>
+            <h5 className="card-title result-card-title">{title}</h5>
+            <p className="card-text result-card-pricing">
+              <span className="me-2 fw-semibold">$ {price}</span>
+              <span className="text-success">60% OFF</span>
+            </p>
           </div>
-        </article>
+        </Link>
+
+        // <article className="col mb-sm-3 container-fluid" key={id}>
+        //   <div
+        //     className="p-0 container-lg product-card-container d-flex justify-content-center align-items-center
+        //    shadow   bg-body rounded
+        //   "
+        //   >
+        //     <Link to={id} style={{ textDecoration: "none", color: "#000" }}>
+        //       <div
+        //         className="card"
+        //         style={{
+        //           minWidth: "250px",
+        //           minHeight: "430px",
+        //           border: "none",
+        //         }}
+        //       >
+        //         <img
+        //           src={img}
+        //           className="card-img-top product-img"
+        //           alt={title}
+        //         />
+        //         <div className="card-body">
+        //           <small className="card-title">{title}</small>
+        //           <p className="pt-1 pb-2 fw-bold card-text product-price">
+        //             &#8377;{price}.00
+        //           </p>
+        //         </div>
+        //       </div>
+        //     </Link>
+        //   </div>
+        // </article>
       )}
     </>
   );
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -17,8 +17,13 @@ export default function Shop() {
             <p className="h2 fw-bolder text-center mt-5">
               <u>Products List</u>
             </p>
-            <div className="container text-center">
-              <div className="row gx-3 row-cols-lg-3 row-cols-md-2 row-cols-sm-1 testing">
+            <div className="container text-center d-flex justify-content-center">
+              {/* row gx-3 row-cols-lg-3 row-cols-md-2 row-cols-sm-1 testing */}
+              <div
+                className="
+              product-list-container
+              "
+              >
                 {products.map((product, index) => (
                   <ProductCard
                     key={index}
